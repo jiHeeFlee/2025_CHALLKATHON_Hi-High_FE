e2d394be22510b6bf7cb472460c11bd51000dd10
jihee <esder1310@naver.com>
2025-06-21 21:26:52 +0900

    feat : #1 landing 페이지 내부 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
-import Image from "next/image";
+'use client';
+
+import styled from '@emotion/styled';
+import { useEffect, useState } from 'react';
+
+import IntroService from '@/components/landing/IntroService';
+import IntroTeam from '@/components/landing/IntroTeam';
+import Login from '@/components/landing/Login';
 
 export default function Home() {
+  const [step, setStep] = useState<'service' | 'team' | 'login'>('service');
+
+  useEffect(() => {
+    if (step === 'service') {
+      const timer = setTimeout(() => {
+        setStep('team');
+      }, 1200);
+      return () => clearTimeout(timer);
+    }
+
+    if (step === 'team') {
+      const timer = setTimeout(() => {
+        setStep('login');
+      }, 1200);
+      return () => clearTimeout(timer);
+    }
+  }, [step]);
   return (
-    <div>
-      <h1>hi</h1>
-    </div>
+    <Container>
+      {step === 'service' && <IntroService />}
+      {step === 'team' && <IntroTeam />}
+      {step === 'login' && <Login />}
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  min-height: 100%;
+
+  background-color: #fff;
+`;
