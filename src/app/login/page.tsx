39b52e3317b9e590f1f6ac6c6e6fc5d16a520eb9
jihee <esder1310@naver.com>
2025-06-21 21:27:20 +0900

    feat : #1 login 페이지 생성

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,18 +2,19 @@
 
 import styled from '@emotion/styled';
 
-import AuthInput from '@/components/input/auth';
+import AuthInput from '@/components/input/AuthInput';
+import BackIcon from '@/assets/BackIcon.svg';
+import LineBar from '@/components/status/LineBar';
+
+import InputInfo from './components/InputInfo';
+import InputEmail from './components/InputEmail';
 
 export default function Login() {
-  return (
-    <Container>
-      <h1>Login page</h1>
-      <AuthInput placeHolder="test" wrongMessage="테스트-틀림" />
-    </Container>
-  );
+  return <Container>로그인</Container>;
 }
 
 const Container = styled.div`
   justify-content: center;
   align-items: center;
+  min-height: 100%;
 `;
