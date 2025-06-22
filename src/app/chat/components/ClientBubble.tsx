f14762d04b2305f2cf1993c6b2c9995260fb1cfd
jihee <esder1310@naver.com>
2025-06-23 02:03:45 +0900

    feat : #4 chat 페이지 내부 채팅 말풍선 구현

--- src/app/chat/components/ClientBubble.tsx
+++ src/app/chat/components/ClientBubble.tsx
@@ -8,20 +8,34 @@ interface ClientBubbleProps {
 }
 
 export default function ClientBubble({ ask_string }: ClientBubbleProps) {
-  return <Container>{ask_string}</Container>;
+  return (
+    <Container>
+      <Wrapper>{ask_string}</Wrapper>
+    </Container>
+  );
 }
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: right;
+  width: 100%;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-content: center;
+  text-align: left;
   max-width: 220px;
+
+  padding: 12px;
 
   color: #fff;
   background-color: var(--primary-color-400);
+  border-radius: 1.25rem;
 
   :hover,
   :active {
     background-color: var(--primary-color-500);
+    transition: all 0.2s ease-in-out;
   }
 `;
--- src/app/chat/components/GptBubble.tsx
+++ src/app/chat/components/GptBubble.tsx
@@ -2,26 +2,40 @@
 
 import styled from '@emotion/styled';
 import { useState } from 'react';
-
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 interface GptBubbleProps {
   answer_string: string;
 }
 
 export default function GptBubble({ answer_string }: GptBubbleProps) {
-  return <Container>{answer_string}</Container>;
+  return (
+    <Container>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{answer_string}</ReactMarkdown>
+    </Container>
+  );
 }
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  max-width: 220px;
+  align-content: center;
+  text-align: left;
+  max-width: 17.8125rem;
+
+  padding: 12px;
 
   color: var(--neutral-color-800);
   background-color: var(--neutral-color-200);
+  border-radius: 1.25rem;
 
   :hover,
   :active {
     background-color: var(--neutral-color-300);
+    transition: all 0.2s ease-in-out;
   }
+`;
+
+const StyledReactMarkdown = styled(ReactMarkdown)`
+  margin: 0;
+  padding: 0;
 `;
