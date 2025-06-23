b06d8fc146425421a26b15a3887cddaf6af12f5a
jihee <esder1310@naver.com>
2025-06-23 22:52:41 +0900

    design/feat : #9 home 페이지 UI 구현 및 기능 구현

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,11 +3,70 @@
 import styled from '@emotion/styled';
 
 import Header from './components/Header';
+import KeywordList from './components/KeywordList';
+import Keyword from './[keyword]/page';
+import { mock } from 'node:test';
+import NavigationBar from './components/NavigationBar';
+
+const moke = {
+  user_name: '승민',
+  date: '2025.06.23 20시 기준',
+  Keyword: '테스트',
+  thumbnail_ur:
+    'https://libapps-au.s3-ap-southeast-2.amazonaws.com/accounts/165593/images/apa7th.png',
+  source: '테스트',
+  title: '테스트중임1. 테스트중임1. 테스트중임1. 테스트중임1'
+};
+
+const mockKeywordList = {
+  keyword: '오늘의테크뉴스',
+  cards: [
+    {
+      thumbnail_url:
+        'https://images.unsplash.com/photo-1581093588401-22d84d84f5e3?auto=format&fit=crop&w=400&q=80',
+      source: 'BBC',
+      title: 'AI가 바꾼 뉴스 제작 현장'
+    },
+    {
+      thumbnail_url:
+        'https://images.unsplash.com/photo-1610484826917-29f643f80cd3?auto=format&fit=crop&w=400&q=80',
+      source: 'CNN',
+      title: 'ChatGPT의 진짜 가능성과 한계'
+    },
+    {
+      thumbnail_url:
+        'https://images.unsplash.com/photo-1556761175-4b46a572b786?auto=format&fit=crop&w=400&q=80',
+      source: 'The Verge',
+      title: '메타버스, 트렌드인가 사라질까?'
+    },
+    {
+      thumbnail_url:
+        'https://images.unsplash.com/photo-1603791452906-c5d7c9c09774?auto=format&fit=crop&w=400&q=80',
+      source: 'Wired',
+      title: '스마트폰 혁신의 끝은 어디인가'
+    },
+    {
+      thumbnail_url:
+        'https://images.unsplash.com/photo-1629442828234-3530f927fbf7?auto=format&fit=crop&w=400&q=80',
+      source: 'TechCrunch',
+      title: '스타트업 투자, 2024 흐름은?'
+    }
+  ]
+};
 
 export default function Home() {
   return (
     <Container>
-      <Header />
+      <Header user_name={moke.user_name} date={moke.date} />
+      <KeywordList
+        keyword={mockKeywordList.keyword}
+        cards={mockKeywordList.cards}
+      />
+      <KeywordList
+        keyword={mockKeywordList.keyword}
+        cards={mockKeywordList.cards}
+      />
+      <NavigationBar />
     </Container>
   );
 }
