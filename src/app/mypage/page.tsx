bbc5df9df0b9ae6494ca15f03cb2f0f31c066340
jihee <esder1310@naver.com>
2025-06-23 22:54:32 +0900

    test : #9 NavigationBar 컴포넌트 테스트를 위한 마이페이지 컴포넌트 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import styled from '@emotion/styled';
+import NavigationBar from '../home/components/NavigationBar';
 
 export default function Mypage() {
   return (
     <Container>
       <h1>Mypage page</h1>
+      <NavigationBar />
     </Container>
   );
 }
