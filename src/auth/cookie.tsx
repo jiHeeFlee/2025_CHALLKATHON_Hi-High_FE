f9ed639d45862a6d278346117a9f61b358978c4f
Seung876 <sm08073469@gmail.com>
2025-06-25 00:12:12 +0900

    feat : iput페이지 연동

--- src/auth/cookie.tsx
+++ src/auth/cookie.tsx
@@ -1,21 +1,17 @@
-'use client';
+'use client'
 
-import { Cookies } from 'react-cookie';
+import { Cookies } from 'react-cookie'
 
-const cookies = new Cookies();
+const cookies = new Cookies()
 
-export const setCookie = (
-  name: string,
-  value: any,
-  options?: Record<string, any>
-): void => {
-  cookies.set(name, value, { path: '/', ...options });
-};
+export const setCookie = (name: string, value: any, options?: Record<string, any>) => {
+  cookies.set(name, value, { path: '/', ...options })
+}
 
 export const getCookie = (name: string): any => {
-  return cookies.get(name);
-};
+  return cookies.get(name)
+}
 
 export const removeCookie = (name: string): void => {
-  cookies.remove(name, { path: '/' });
-};
+  cookies.remove(name, { path: '/' })
+}
