49ffad80979d9242ed47e6b933a0577e7a312abb
jihee <esder1310@naver.com>
2025-06-26 00:38:19 +0900

    design : #23 GptBubble.tsx 마크다운 라이브러리 정렬 수정

--- src/app/chat/components/GptBubble.tsx
+++ src/app/chat/components/GptBubble.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import styled from '@emotion/styled';
-import { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+
 interface GptBubbleProps {
   answer_string: string;
 }
@@ -18,6 +18,7 @@ export default function GptBubble({ answer_string }: GptBubbleProps) {
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   align-content: center;
   text-align: left;
   width: fit-content;
@@ -29,14 +30,11 @@ const Container = styled.div`
   background-color: var(--neutral-color-200);
   border-radius: 1.25rem;
 
+  line-height: 175%;
+
   :hover,
   :active {
     background-color: var(--neutral-color-300);
     transition: all 0.2s ease-in-out;
   }
-`;
-
-const StyledReactMarkdown = styled(ReactMarkdown)`
-  margin: 0;
-  padding: 0;
 `;
